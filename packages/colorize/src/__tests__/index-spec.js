7a9c39e95874bc02b9be16f4ab43d13449be3678
canastro <ricardocanastro@gmail.com>
2018-06-03 21:37:50 +0100

    :sparkles: refactor colorize

---
+++ packages/colorize/src/__tests__/index-spec.js
@@ -0,0 +1,72 @@
+import core from 'image-filter-core';
+import victim, { transform } from '../index';
+
+jest.mock('image-filter-core');
+
+describe('colorize', () => {
+    describe('when data is not defined', () => {
+        it('should throw error', () => {
+            expect(() => victim()).toThrowError(
+                'image-filter-colorize:: invalid options provided'
+            );
+        });
+    });
+
+    describe('when options is not defined', () => {
+        it('should throw error', () => {
+            expect(() => victim({})).toThrowError(
+                'image-filter-colorize:: invalid options provided'
+            );
+        });
+    });
+
+    describe('when color is not defined', () => {
+        it('should throw error', () => {
+            expect(() => victim({}, {})).toThrowError(
+                'image-filter-colorize:: invalid options provided'
+            );
+        });
+    });
+
+    describe('when level is not defined', () => {
+        it('should throw error', () => {
+            expect(() => victim({}, { color: '#FFF' })).toThrowError(
+                'image-filter-colorize:: invalid options provided'
+            );
+        });
+    });
+
+    describe('when has all paramters', () => {
+        let result;
+        const data = 'DATA';
+        const options = { color: '#FFF', level: 50 };
+
+        beforeAll(() => {
+            core.applyFilter = jest.fn().mockReturnValue('MOCK-VALUE');
+            result = victim(data, options, 4);
+        });
+
+        it('should call applyFilter', () => {
+            expect(core.applyFilter).toHaveBeenCalledWith(
+                data,
+                expect.anything(),
+                options,
+                4
+            );
+        });
+
+        it('should return the applyFilter result', () => {
+            expect(result).toEqual('MOCK-VALUE');
+        });
+    });
+});
+
+describe('#transform()', function() {
+    it('should apply transformation and return as imageData', function() {
+        const data = [193, 219, 242, 255, 193, 219, 242, 255];
+        const expectedData = [96.5, 173.5, 185, 255, 193, 219, 242, 255];
+
+        transform(data, 4, { color: '#008080', level: 50 });
+        expect(data).toEqual(expectedData);
+    });
+});
--- packages/colorize/src/index.js
+++ packages/colorize/src/index.js
@@ -1,8 +1,31 @@
-var imageFilterCore = require('image-filter-core');
-var transform = require('./transform');
+import { applyFilter } from 'image-filter-core';
 
 /**
- * @name colorize
+ * @param {Object} data
+ * @param {Number} length
+ * @param {Object} options
+ * @param {Number} [options.color]
+ * @param {Number} [options.level]
+ */
+export const transform = (data, length, options) => {
+    const hex =
+        options.color.charAt(0) === '#'
+            ? options.color.substr(1)
+            : options.color;
+    const colorRGB = {
+        r: parseInt(hex.substr(0, 2), 16),
+        g: parseInt(hex.substr(2, 2), 16),
+        b: parseInt(hex.substr(4, 2), 16)
+    };
+
+    for (let i = 0; i < length; i += 4) {
+        data[i] -= (data[i] - colorRGB.r) * (options.level / 100);
+        data[i + 1] -= (data[i + 1] - colorRGB.g) * (options.level / 100);
+        data[i + 2] -= (data[i + 2] - colorRGB.b) * (options.level / 100);
+    }
+};
+
+/**
  * @param {ImageData} data - data of a image extracted from a canvas
  * @param {Object} options - options to pass to the transformation function
  * @param {Number} [options.color] - color value to apply in the transformation
@@ -10,10 +33,10 @@ var transform = require('./transform');
  * @param {Number} nWorkers - number of workers
  * @returns {Promise}
  */
-module.exports =  function colorize(data, options, nWorkers) {
+export default function colorize(data, options, nWorkers) {
     if (!data || !options || !options.color || !options.level) {
         throw new Error('image-filter-colorize:: invalid options provided');
     }
 
-    return imageFilterCore.apply(data, transform, options, nWorkers);
-};
+    return applyFilter(data, transform, options, nWorkers);
+}
