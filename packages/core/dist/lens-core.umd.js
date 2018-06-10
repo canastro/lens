d1fcf26593b347aae936e9855c375cfa65fc3678
canastro <ricardocanastro@gmail.com>
2018-06-10 11:35:33 +0100

    test

--- packages/core/dist/lens-core.umd.js
+++ packages/core/dist/lens-core.umd.js
@@ -1,17 +1,55 @@
 !(function(t, e) {
     'object' == typeof exports && 'undefined' != typeof module
-        ? e(require('workerize'))
+        ? e()
         : 'function' == typeof define && define.amd
-            ? define(['workerize'], e)
-            : e(t.workerize);
-})(this, function(a) {
+            ? define(e)
+            : e();
+})(0, function() {
     'use strict';
-    function o(t, e) {
+    function h(o, i, s) {
+        o.addEventListener('message', function(t) {
+            var e = t.data,
+                n = e.id;
+            if ('RPC' === e.type && null != n)
+                if (e.method) {
+                    var r = i[e.method];
+                    null == r
+                        ? o.postMessage({
+                              type: 'RPC',
+                              id: n,
+                              error: 'NO_SUCH_METHOD'
+                          })
+                        : Promise.resolve()
+                              .then(function() {
+                                  return r.apply(null, e.params);
+                              })
+                              .then(function(t) {
+                                  o.postMessage({
+                                      type: 'RPC',
+                                      id: n,
+                                      result: t
+                                  });
+                              })
+                              .catch(function(t) {
+                                  o.postMessage({
+                                      type: 'RPC',
+                                      id: n,
+                                      error: '' + t
+                                  });
+                              });
+                } else {
+                    var a = s[n];
+                    if (null == a) throw Error('Unknown callback ' + n);
+                    delete s[n],
+                        e.error ? a[1](Error(e.error)) : a[0](e.result);
+                }
+        });
+    }
+    function r(t, e) {
         var n = document.createElement('canvas');
         return (n.width = t), (n.height = e), n;
     }
-    (a = a && a.hasOwnProperty('default') ? a.default : a),
-        (exports.getCanvas = o),
+    (exports.getCanvas = r),
         (exports.convertImageDataToCanvasURL = function(t) {
             var e = window.document.createElement('canvas'),
                 n = e.getContext('2d');
@@ -26,25 +64,109 @@
             var e = t.data,
                 n = t.transform,
                 i = t.options,
-                d = t.nWorkers,
-                h = a(
+                s = t.nWorkers,
+                c = (function(t, e) {
+                    var n,
+                        o,
+                        i,
+                        r = this,
+                        a = {},
+                        s =
+                            '__xpo' +
+                            Math.random()
+                                .toString()
+                                .substring(2) +
+                            '__';
+                    'function' == typeof t &&
+                        (t =
+                            '(' +
+                            Function.prototype.toString.call(t) +
+                            ')(' +
+                            s +
+                            ')'),
+                        (o = s),
+                        (i = a),
+                        (n = (n = (n = t).replace(
+                            /^(\s*)export\s+default\s+/m,
+                            function(t, e) {
+                                return (
+                                    (i.default = !0), '' + e + o + '.default='
+                                );
+                            }
+                        )).replace(
+                            /^(\s*)export\s+((?:async\s*)?function(?:\s*\*)?|const|let|var)(\s+)([a-zA-Z$_][a-zA-Z0-9$_]*)/gm,
+                            function(t, e, n, r, a) {
+                                return (
+                                    (i[a] = !0),
+                                    '' + e + o + '.' + a + '=' + n + r + a
+                                );
+                            }
+                        )),
+                        (t =
+                            'var ' +
+                            o +
+                            '={};\n' +
+                            n +
+                            '\n' +
+                            o +
+                            ';\n(' +
+                            Function.prototype.toString.call(h) +
+                            ')(self,' +
+                            s +
+                            ',{})');
+                    var c,
+                        u = URL.createObjectURL(new Blob([t])),
+                        l = new Worker(u, e),
+                        p = l.terminate,
+                        d = {},
+                        f = 0;
+                    for (c in ((l.kill = function(t) {
+                        l.postMessage({ type: 'KILL', signal: t }),
+                            setTimeout(l.terminate);
+                    }),
+                    (l.terminate = function() {
+                        URL.revokeObjectURL(u), p.call(r);
+                    }),
+                    (l.call = function(r, a) {
+                        return new Promise(function(t, e) {
+                            var n = 'rpc' + ++f;
+                            (d[n] = [t, e]),
+                                l.postMessage({
+                                    type: 'RPC',
+                                    id: n,
+                                    method: r,
+                                    params: a
+                                });
+                        });
+                    }),
+                    (l.rpcMethods = {}),
+                    h(l, l.rpcMethods, d),
+                    (l.expose = function(t) {
+                        l[c] = function() {
+                            return l.call(t, [].slice.call(arguments));
+                        };
+                    }),
+                    a))
+                        c in l || l.expose(c);
+                    return l;
+                })(
                     '\n        var transform = ' +
                         n +
                         ';\n\n        export function execute(canvas, index, length, options) {\n            canvas.data = transform({ \n                data: canvas.data, \n                length: length, \n                options: options\n            });\n\n            return { result: canvas, index: index };\n        }\n    '
                 ),
-                u = o(e.width, e.height),
-                s = u.getContext('2d');
-            s.putImageData(e, 0, 0), (d = d || 1);
-            var c = Math.floor(u.height / d);
+                u = r(e.width, e.height),
+                l = u.getContext('2d');
+            l.putImageData(e, 0, 0), (s = s || 1);
+            var p = Math.floor(u.height / s);
             return new Promise(function(e) {
-                for (var n = 0, t = void 0, a = 0; a < d; a++) {
-                    (t = c), a + 1 === d && (t = u.height - c * a);
-                    var o = s.getImageData(0, c * a, u.width, t),
-                        r = t * u.width * 4;
-                    h.execute(o, a, r, i).then(function(t) {
-                        s.putImageData(t.result, 0, c * t.index),
-                            ++n === d &&
-                                e(s.getImageData(0, 0, u.width, u.height));
+                for (var n = 0, t = void 0, r = 0; r < s; r++) {
+                    (t = p), r + 1 === s && (t = u.height - p * r);
+                    var a = l.getImageData(0, p * r, u.width, t),
+                        o = t * u.width * 4;
+                    c.execute(a, r, o, i).then(function(t) {
+                        l.putImageData(t.result, 0, p * t.index),
+                            ++n === s &&
+                                e(l.getImageData(0, 0, u.width, u.height));
                     });
                 }
             });
--- packages/x-lens/src/components/x-lens/x-lens.tsx
+++ packages/x-lens/src/components/x-lens/x-lens.tsx
@@ -1,6 +1,29 @@
 import { Component, Prop, State } from '@stencil/core';
 import { convertImageDataToCanvasURL } from 'lens-core';
 import brightness from 'lens-filter-brightness';
+import color from 'lens-filter-color';
+import colorize from 'lens-filter-colorize';
+import contrast from 'lens-filter-contrast';
+import gamma from 'lens-filter-gamma';
+import grayscale from 'lens-filter-grayscale';
+import invert from 'lens-filter-invert';
+import noise from 'lens-filter-noise';
+import sepia from 'lens-filter-sepia';
+import threshold from 'lens-filter-threshold';
+
+const METHODS = {
+    brightness,
+    color,
+    colorize,
+    contrast,
+    gamma,
+    grayscale,
+    invert,
+    noise,
+    sepia,
+    threshold
+};
+
 
 @Component({
   tag: 'x-lens',
@@ -16,6 +39,7 @@ export class XLens {
 
   componentDidLoad() {
     this.isLoading = true;
+    const filter = METHODS[this.filter];
 
     const img = new Image();
     img.crossOrigin = 'Anonymous';
@@ -30,7 +54,7 @@ export class XLens {
 
         const data = context.getImageData(0, 0, img.width, img.height);
 
-        return brightness({ data, options: { level: this.level }, nWorkers: 4 })
+        return filter({ data, options: { level: this.level }, nWorkers: 4 })
           .then(results => { this.data = convertImageDataToCanvasURL(results); })
           .then(() => {
             this.isLoading = false;
